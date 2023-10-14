713b55f302390d0e2b62e5ce8e94488faeca3460
Павлова Яна <YYNNAA2010@mail.ru>
2023-10-14 20:07:51 +0500

    Изменение в INSTALLED_APPS, добавление SITE_ID

--- Travel/settings.py
+++ Travel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-46cubb=o!@j@df(ft)8s5^4plezmsna919g&z%nrrgs4$9b*pf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pereval.apps.PerevalConfig',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
