8f9862603ab68cd76ee54fdd3645ef356f88742e
Павлова Яна <YYNNAA2010@mail.ru>
2023-10-14 19:59:05 +0500

    Зарегистрированы модели Tourist, Coordinates, Level, Pereval, Photos.

--- pereval/admin.py
+++ pereval/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Tourist, Coordinates, Photos, Level, Pereval
 
-# Register your models here.
+admin.site.register(Tourist)
+admin.site.register(Coordinates)
+admin.site.register(Photos)
+admin.site.register(Level)
+admin.site.register(Pereval)
+
